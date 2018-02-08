0ee5ab3864ffdb2310b593b22d4856631cb3eaf7
Evgeni Dinev <ewgeni.dinew@gmail.com>
2018-02-08 21:43:56 +0200

    --added files(tasks,solutions,etc...)

---
+++ C_Sharp-Advanced/04.FunctionalProgr/02.Knights/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02.Knights
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(new char[] {' '},StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> print = (line) => Console.WriteLine($"Sir {line}");
+
+            foreach (var word in input)
+            {
+                print(word);
+            }
+        }
+    }
+}
---
+++ C_Sharp-Advanced/04.FunctionalProgr/04.EvenOdd/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.EvenOdd
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var command = Console.ReadLine()
+                .ToLower();
+
+            var lowerBound = input[0];
+            var upperBound = input[1];
+
+            Predicate<int> predicate = (num) => num%2==0;
+
+            switch (command)
+            {
+                case "even": PrintEvenNum(lowerBound,upperBound,predicate); break;
+                case "odd": PrintOddNum(lowerBound,upperBound,predicate); break;
+            }
+        }
+
+        private static void PrintOddNum(int lowerBound, int upperBound, Predicate<int> predicate)
+        {
+            for (int i = lowerBound; i <=upperBound; i++)
+            {
+                if (!predicate(i))
+                {
+                    Console.Write(i+" ");
+                }
+            }
+        }
+
+        private static void PrintEvenNum(int lowerBound, int upperBound, Predicate<int> predicate)
+        {
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                if (predicate(i))
+                {
+                    Console.Write(i +" ");
+                }
+            }
+        }
+    }
+}
